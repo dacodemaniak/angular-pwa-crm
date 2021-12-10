98fbf523c5f40c3bcec4c8ff73622b2c76590519
Jean-Luc <jean-luc.a@ideafactory.fr>
2021-12-10 16:32:00 +0100

    Add contact form

--- src/app/contact/pages/add-contact/add-contact.component.ts
+++ src/app/contact/pages/add-contact/add-contact.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contact',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-contact.component.scss']
 })
 export class AddContactComponent implements OnInit {
+  public contactForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpClient: HttpClient
+  ) {}
 
   ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      lastName: [
+        '',
+        Validators.required
+      ],
+      firstName: [
+        '',
+        Validators.required
+      ]
+    })
+  }
+
+  public onSubmit(): void {
+    if (this.contactForm.valid) {
+      this.httpClient.post<any>(
+        'http://localhost:5000/person',
+        this.contactForm.value,
+        {
+          observe: 'response'
+        }
+      ).subscribe((response: HttpResponse<any>) => {
+        console.log(JSON.stringify(response));
+      })
+    }
   }
 
 }
